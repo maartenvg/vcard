e2cbc76dfec721347698d6fcfbbdd51f678b1246
maartenvg <git@maartenvangrootel.nl>
2013-03-22 17:09:58 -0400

    Further improvement of the lay-out.

--- config.rb
+++ config.rb
@@ -8,12 +8,13 @@ css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
-
+fonts_dir = "fonts"
+fonts_path = "fonts"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+# relative_assets = false
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
