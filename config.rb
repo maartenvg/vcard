bdbca33ed0471ceaab76b07518df2abe3fde779f
maartenvg <git@maartenvangrootel.nl>
2013-03-29 15:18:52 -0400

    Config for root

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'zurb-foundation'
 
 
 # Set this to the root of your project when deployed:
-http_path = "/vcard/"
+http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
