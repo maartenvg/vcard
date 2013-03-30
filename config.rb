7fc1857b8e38c2b7c6395622894ac47dd0b520c7
maartenvg <git@maartenvangrootel.nl>
2013-03-30 09:19:49 -0400

    little clean-up

--- config.rb
+++ config.rb
@@ -13,11 +13,10 @@ fonts_dir = "fonts"
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+relative_assets = true  
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
